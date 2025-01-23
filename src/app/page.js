965da5af93febbf6a7456f4f1ead1aa330eddf63
MH Code <mazheriftikhar1@gmail.com>
2025-01-23 13:49:37 +0500

    23-Jan

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return(
-  <h1 className="font-bold text-2xl">Welcome to next Js</h1>
+  <>
+    <h1 className="font-bold text-2xl">Welcome to next Js</h1>
+    <nav>
+        <Link href="/about">Go to About Page</Link>
+      </nav>
+  </>
+  
   );
 }
