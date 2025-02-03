d3be63f1e20c0bab61c96b8bf1b5f302ac113761
MH Code <mazheriftikhar1@gmail.com>
2025-02-03 14:16:11 +0500

    3-Feb

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import Button from "./components/Button";
 
 export default function Home() {
   return(
   <>
     <h1 className="font-bold text-2xl">Welcome to next Js</h1>
-    <nav>
-        <Link href="/about">Go to About Page</Link>
-      </nav>
+      <p>Welcome to the homepage!</p>
+      <Link href="/about">Go to About Page</Link> {/* Link to About page */}
+      <Button text="Click Me" />
   </>
   
   );
